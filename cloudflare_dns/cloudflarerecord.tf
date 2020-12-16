294e8d287f1110a47adbf5c937d66570dc13a3ac
Jeremy Woods <jwoods@ssbinfo.com>
2020-12-15 17:03:22 -0700

    Added option for more dns

--- cloudflare_dns/cloudflarerecord.tf
+++ cloudflare_dns/cloudflarerecord.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "record" {
   for_each = var.dnsentries
   zone_id  = data.cloudflare_zones.zone.zones[0].id
-  name     = each.key
+  name     = each.value.dnsname
   value    = each.value.dnsvalue
   type     = each.value.dnstype
   proxied  = true
--- cloudflare_dns/terraform.tfvars
+++ cloudflare_dns/terraform.tfvars
@@ -5,7 +5,13 @@ dnstype  = "A"
 
 dnsentries = {
 #   "www" = {
+#     dnsname  = "www"
 #     dnsvalue = "73.169.93.248"
+#     dnstype  = "A"
+#   }
+#   "www-asia" = {
+#     dnsname  = "www"
+#     dnsvalue = "198.51.100.15"
 #     dnstype  = "A"
 #   }
 }
